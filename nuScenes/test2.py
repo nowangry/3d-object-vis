dcace2ad812542fa4cd29ce43a25c3f6c2db0193
nowangry <954600581@qq.com>
2023-02-21 15:03:30 +0800

    nuScenes

--- nuScenes/test2.py
+++ nuScenes/test2.py
@@ -12,6 +12,19 @@ vis.get_render_option().load_from_json("render_option.json")
 num_frames = 10
 
 for i in range(num_frames):
+    # 调整视角
+    view_control = vis.get_view_control()
+    view_control.rotate(30.0, 20.0)
+    view_control.translate(-1.0, 0.5, 0.0)
+
+    # 转化为相机参数并保存到文件
+    param = view_control.convert_to_pinhole_camera_parameters()
+    o3d.io.write_pinhole_camera_parameters('view.json', param)
+    # 加载视角
+    params = o3d.io.read_pinhole_camera_parameters('camera.json')
+    view_ctrl = vis.get_view_control()
+    view_ctrl.convert_from_pinhole_camera_parameters(params)
+
     # 更新场景和渲染器
     vis.update_geometry(pcd)
     vis.poll_events()
